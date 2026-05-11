6aafd4f01ff51ec2fcbad66e33c09dfb33f6ba06
benjaminyzy <byeh5665@gmail.com>
2026-05-12 00:34:30 +0800

    feat: add centralized keyword config

---
+++ src/keywords.py
@@ -0,0 +1,36 @@
+"""Centralized keyword lists for feature extraction and rule engine."""
+
+SUSPICIOUS_KEYWORDS = [
+    "login", "verify", "secure", "account", "update",
+    "banking", "confirm", "password", "signin", "session",
+    "wallet", "authenticate", "validation",
+]
+
+BRAND_KEYWORDS = [
+    "paypal", "amazon", "apple", "microsoft", "google",
+    "facebook", "netflix", "instagram", "linkedin",
+    "whatsapp", "twitter", "dropbox", "github",
+    "maybank", "cimb", "publicbank", "rhb", "hsbc",
+]
+
+PRIZE_KEYWORDS = [
+    "prize", "winner", "claim", "reward", "lucky",
+    "congratulations", "youwon", "you-won", "limited-offer",
+    "free-gift", "free-prize",
+]
+
+SHORTENER_DOMAINS = {
+    "bit.ly", "tinyurl.com", "goo.gl", "t.co", "ow.ly",
+    "is.gd", "buff.ly", "rebrand.ly", "cutt.ly",
+    "shorturl.at", "tiny.cc", "bl.ink", "lnkd.in",
+    "tr.im", "x.co", "mcaf.ee",
+}
+
+# TLDs heavily abused for phishing/malware per Spamhaus and APWG reports
+SUSPICIOUS_TLDS = {
+    "tk", "ml", "ga", "cf", "gq",
+    "xyz", "top", "loan", "click",
+    "work", "country", "stream", "download",
+    "racing", "win", "bid", "trade", "date",
+    "review", "party", "men", "gdn",
+}
